200742245fcd0ba61d72b03685d982f60989c77b
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-04-29 14:45:14 +0545

    more map examples.

--- collections/MapExample.java
+++ collections/MapExample.java
@@ -11,8 +11,25 @@ public class MapExample {
         map.put("ram", 2);
         map.put("shyam", 22);
 
-        System.out.println(map.get("ram"));
-        System.out.println(map.get("shyam"));
-        System.out.println(map.size());
+        // System.out.println(map.get("ram"));
+        // System.out.println(map.get("shyam"));
+        // System.out.println(map.size());
+
+        // check whether key exists or not
+        System.out.println(map.containsKey("ram")); // will return true because `ram` exists in the map
+
+        System.out.println(map.containsKey("shyamkumar")); // will return false because `shyamkumar` does not exist in the map
+
+        // map.clear();
+
+        System.out.println(map.size() + " is the size of the map after clearing.");
+
+        Map<String, Integer> students = new HashMap<>();
+        students.putAll(map);
+
+        System.out.println(students.size());
+
+
+        
     }
 }
